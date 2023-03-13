39189f01faed9f4d1fa0cffaccfd34b059976f79
Nikolai Serafimovich <serafimo2001@gmail.com>
2023-03-13 21:31:18 +0200

    read files directly

--- main.c
+++ main.c
@@ -1,11 +1,25 @@
-//version 1.2
+/*
+Copyright (C) 2023 Nikolai Serafimovich
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+*/
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
 
-#define HELP_TXT "hexdiff v1.2\nUsage: hexdiff <file1> <file2>\n"
+#define HELP_TXT "hexdiff v1.3\nUsage: hexdiff <file1> <file2>\n"
 #define FILE_OPEN_ERROR "Error while opening the file.\n"
+
+#define PRINT_DOUBLE_HEX "%08X: %02X %02X\n"
 
 bool check_arg(int argc, char *argv[])
 {
@@ -26,6 +40,12 @@ int fsize(FILE *fp)
     return sz;
 }
 
+struct file {
+    char *name;
+    unsigned long size;
+    FILE *file;
+};
+
 int main(int argc, char *argv[])
 {
     if (!check_arg(argc, argv))
@@ -34,99 +54,54 @@ int main(int argc, char *argv[])
         return 0;
     }
 
-    const char *fname1 = argv[1];
-    const char *fname2 = argv[2];
+    struct file f_a;
+    struct file f_b;
 
-    FILE *f1 = fopen(fname1, "rb");
-    FILE *f2 = fopen(fname2, "rb");
+    f_a.name = argv[1];
+    f_b.name = argv[2];
 
-    if (f1 == NULL || f2 == NULL)
+    f_a.file = fopen(f_a.name, "rb");
+    f_b.file = fopen(f_b.name, "rb");
+
+    if (f_a.file == NULL || f_b.file == NULL)
     {
         perror(FILE_OPEN_ERROR);
         exit(EXIT_FAILURE);
     }
 
-    const unsigned long fsize1 = fsize(f1);
-    const unsigned long fsize2 = fsize(f2);
+    f_a.size = fsize(f_a.file);
+    f_b.size = fsize(f_b.file);
 
     unsigned char *data1;
     unsigned char *data2;
 
-    data1 = (char*) malloc(fsize1 * sizeof(char));
-    data2 = (char*) malloc(fsize2 * sizeof(char));
+    data1 = (char*) malloc(f_a.size * sizeof(char));
+    data2 = (char*) malloc(f_b.size * sizeof(char));
 
-
-    if(fsize1 == fsize2)
+    if(f_a.size == f_b.size)
     {
-        for ( int i = 0 ; i < fsize1 ; i++ ) 
+        for ( int i = 0 ; i < f_a.size; i++ ) 
         {
-            fread(&data1[i], sizeof(data1[i]), 1, f1);
-            fread(&data2[i], sizeof(data2[i]), 1, f2);
-        }
+            unsigned char data[2];
 
-        for (int i = 0; i < fsize1; i++)
-        {
-            if (data1[i] != data2[i])
+            fread(&data[0], 1, 1, f_a.file);
+            fread(&data[1], 1, 1, f_b.file);
+
+            if (data[0] != data[1])
             {
-                printf("%08X: %02X %02X\n", i, data1[i], data2[i]);
-            }
-        }
-        fclose(f1);
-    }
-
-    if(fsize1 < fsize2)
-    {
-        for ( int i = 0 ; i < fsize1 ; i++ ) 
-        {
-            fread(&data1[i], sizeof(data1[i]), 1, f1);
-        }
-
-        for ( int i = 0 ; i < fsize2 ; i++ )
-        {
-            fread(&data2[i], sizeof(data2[i]), 1, f2);
-        }
-
-        for (int i = 0; i < fsize1; i++)
-        {
-            if (data1[i] != data2[i])
-            {
-                printf("%08X: %02X %02X\n", i, data1[i], data2[i]);
+                printf(PRINT_DOUBLE_HEX, i, data[0], data[1]);
             }
         }
 
-        printf("%d more bytes in %s:\n", fsize2 - fsize1, fname2);
-        for (int i = fsize1; i < fsize2; i++)
-        {
-            printf("%08X: %02X\n", i, data2[i]);
-        }
-
-    }
-
-    if(fsize1 > fsize2)
-    {
-        for ( int i = 0 ; i < fsize1 ; i++ ) 
-        {
-            fread(&data1[i], sizeof(data1[i]), 1, f1);
-        }
-
-        for ( int i = 0 ; i < fsize2 ; i++ )
-        {
-            fread(&data2[i], sizeof(data2[i]), 1, f2);
-        }
-
-        for (int i = 0; i < fsize2; i++)
+        for (int i = 0; i < f_a.size; i++)
         {
             if (data1[i] != data2[i])
             {
-                printf("%08X: %02X %02X\n", i, data1[i], data2[i]);
+                printf(PRINT_DOUBLE_HEX, i, data1[i], data2[i]);
             }
         }
-
-        printf("%d more bytes in %s:\n", fsize1 - fsize2, fname1);
-        for (int i = fsize2; i < fsize1; i++)
-        {
-            printf("%08X: %02X\n", i, data1[i]);
-        }
+        fclose(f_a.file);
+        fclose(f_b.file);
     }
 
     return 0;
